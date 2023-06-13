873f2fbfa7b21b4709875571746121aaf324f432
A-rielox <arielox.ag@gmail.com>
2023-06-13 11:28:57 -0600

    46

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers;
@@ -19,8 +20,87 @@ public class CategoryController : Controller
 	////////////////////////////////////////////////
 	public IActionResult Index()
 	{
-		var categories = _db.Categories.ToList();
+		List<Category> categories = _db.Categories.ToList();
 
 		return View(categories);
 	}
+
+
+	////////////////////////////////////////////////
+	////////////////////////////////////////////////
+	public IActionResult Create()
+	{
+		return View();
+	}
+
+	[HttpPost]
+	public IActionResult Create(Category obj)
+	{
+		// p' una validacion custom ( en este caso p'q no sean iguales )
+		if (obj.Name == obj.DisplayOrder.ToString())
+		{
+			// la key ( el 1er valor es p' q se agrege a la validacion de ese field en el modelo
+			// en este caso va a aparecer como error en el Name 
+			ModelState.AddModelError("name", "The display order cannot be equal to the name");
+		}
+		if (obj.Name != null && obj.Name.ToLower() == "test")
+		{
+			// en este caso (con la key vacia) se va a agregar a la validacion del modelo
+			// y va a aparecer en la validacion gral <div asp-validation-summary="All"></div> 
+			ModelState.AddModelError("", "el nombre no puede ser test");
+		}
+
+		if (ModelState.IsValid)
+		{
+			_db.Categories.Add(obj);
+			_db.SaveChanges();
+
+			return RedirectToAction("Index");
+
+		}
+
+		return View(); // si no es valido el modelo me deja en el mismo lado
+
+
+		// si quiera ir a otro controller seria:
+		// RedirectToAction("nombre_accion","nombre_controller");
+	}
+
+
+	////////////////////////////////////////////////
+	////////////////////////////////////////////////
+	public IActionResult Edit(int? id)
+	{
+		if (id == null || id == 0) return NotFound(); // aca podria enviar a una error page p.ej.
+
+		var category = _db.Categories.Find(id);
+
+		if (category == null) return NotFound();
+
+		return View(category);
+	}
+
+	[HttpPost]
+	public IActionResult Edit(Category obj)
+	{
+		if (ModelState.IsValid)
+		{
+			_db.Categories.Add(obj);
+			_db.SaveChanges();
+
+			return RedirectToAction("Index");
+
+		}
+
+		return View();
+	}
+
+
+
+
+
+
+
+
+
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models;
 
@@ -7,8 +8,13 @@ public class Category
     // [Key]
     public int Id { get; set; }
 
+
     [Required]
-    public string Name { get; set; }
-    
+    [MaxLength(30)]
+	[DisplayName("Category Name")]
+	public string Name { get; set; }
+
+    [Range(1,100,ErrorMessage = "El valor debe ser entre {1} y {2}")]
+    [DisplayName("Display Order")]
     public int DisplayOrder { get; set; } 
 }
